a1e7130aae2bb3f45e933cade903d6d084dd5859
Kyle Hernandez <kmhernan84@gmail.com>
2013-04-12 18:29:06 -0500

    adding option parsing

--- src/com/lib/OptionParser.scala
+++ src/com/lib/OptionParser.scala
@@ -4,6 +4,23 @@
  *
  */
 
-package com.lib.OptionParser
+package com.lib.argparse
 
+abstract class AttributeHolder {
+  def repr:
 
+class OptionParser {
+  private var options = Map[String, Argument]
+
+  def add_argument(name: String, value: Any, help: String): Unit = {
+    options ++ Map(name, Argument(name, value, help))
+  }
+
+  def print: Unit = {
+    println(options)
+  }
+} 
+
+class Argument(name: String, value: Any, help: String) {
+   
+} 
