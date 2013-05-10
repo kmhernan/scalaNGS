fd956e0a7a3ee9465c7c0afe536612e6e68cd4c0
Kyle Hernandez <kmhernan84@gmail.com>
2013-05-10 17:12:20 -0500

    working on plotting

--- src/main/scala/com/kmh/ngs/plotting/ReadStatsPlots.scala
+++ src/main/scala/com/kmh/ngs/plotting/ReadStatsPlots.scala
@@ -3,13 +3,4 @@ import scala.sys.process._
 
 class QualityBoxWhiskersPlot {
   val cmdPrefix = "-e "
-  val cmdContainer = new ListBuffer[String]
 }
-
-object QualityBoxWhiskersPlots {
-  def apply = {
-    val qPlot = new QualityBoxWhiskersPlot
-    qPlot.add()
-  }
-
-} 
